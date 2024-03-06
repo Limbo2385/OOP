245fc20774809008eb270a4e7d0a849c2885796a
= <svilal081@gmail.com>
2024-03-06 16:48:22 +0300

    lab3 done

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -9,14 +9,24 @@ class Organization
 {
 private:
     string organization;
+    string date;
 public:
     string getOrganization() { return organization; }
 
     void setOrganization(string organization) { this->organization = organization; }
 
+    string getDate() { return date; }
+
+    void setDate(string date) { this->date = date; }
+
     void showOrganization()
     {
         cout << "Организация: " << getOrganization() << endl;
+    }
+
+    void showDate() 
+    {
+        cout << "Дата создания: " << getDate() << endl;
     }
 };
 
@@ -41,10 +51,11 @@ private:
     string nameFactory;
     int performance;
 public:
-    Factory(string organization, string insuranceCompany, string nameFactory, int performance)
+    Factory(string organization, string insuranceCompany, string nameFactory, int performance, string date)
     {
         setOrganization(organization);
         setInsuranceCompany(insuranceCompany);
+        setDate(date);
         this->nameFactory = nameFactory;
         this->performance = performance;
     }
@@ -59,8 +70,65 @@ public:
     {
         showOrganization();
         showInsuranceCompany();
+        showDate();
         cout << "Завод: " << getNameFactory() << endl;
         cout << "Производительность завода: " << getPerformance() << endl << endl;
+    }
+};
+
+class Person {
+private:
+    string name;
+    int age;
+public:
+    void setName(string name) { this->name = name; }
+    void setAge(int age) { this->age = age; }
+    string getName() {return name; }
+    int getAge() { return age; }
+};
+
+
+class Student : public Person {
+private:
+    int kurs;
+public:
+    Student(string name, int age, int kurs) {
+        setName(name);
+        setAge(age);
+        this->kurs = kurs;
+    }
+    void setKurs(int kurs) {
+        this->kurs = kurs;
+    }
+
+    int getKurs() { return kurs; }
+
+    void showStudent() {
+        cout << "Имя: " << getName() << endl;
+        cout << "Возраст: " << getAge() << endl;
+        cout << "Курс: " << kurs << endl;
+    }
+};
+
+class Teacher : public Person {
+private:
+    string lesson;
+public:
+    Teacher(string name, int age, string lesson) {
+        setName(name);
+        setAge(age);
+        this->lesson = lesson;
+    }
+    void setLesson(int lesson) {
+        this->lesson = lesson;
+    }
+
+    string getLesson() { return lesson; }
+
+    void showLesson() {
+        cout << "Имя: " << getName() << endl;
+        cout << "Возраст: " << getAge() << endl;
+        cout << "Что ведет: " << lesson << endl;
     }
 };
 
@@ -72,19 +140,27 @@ int main()
 
     string organization;
     string insuranceCompany;
+    string date;
     string nameFactory;
     int performance;
     vector<Factory> v1;
+
+    string name;
+    int age;
+    int kurs;
+    string lesson;
+
 
     for (size_t i = 0; i < 2; i++)
     {
         cout << "Введите организацию: "; cin >> organization;
         cout << "Страховую компанию: ";    cin >> insuranceCompany;
+        cout << "Введите дату создания: "; cin >> date;
         cout << "Название завода: ";    cin >> nameFactory;
         cout << "Производительность завода: ";    cin >> performance;
         cout << endl;
 
-        Factory factory{ organization, insuranceCompany, nameFactory, performance };
+        Factory factory{ organization, insuranceCompany, nameFactory, performance, date };
         v1.push_back(factory);
     }
 
@@ -92,4 +168,20 @@ int main()
     {
         v1[i].show();
     }
+
+    cout << "Введите имя: "; cin >> name;
+    cout << "Введите возраст: ";    cin >> age;
+    cout << "Введите курс: "; cin >> kurs;
+
+    Student student = { name, age, kurs };
+    cout << endl;
+    student.showStudent();
+
+    cout << "Введите имя: "; cin >> name;
+    cout << "Введите возраст: ";    cin >> age;
+    cout << "Что ведёт: "; cin >> lesson;
+
+    Teacher teacher = { name, age, lesson };
+    cout << endl;
+    teacher.showLesson();
 }
