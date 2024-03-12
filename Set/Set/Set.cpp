67d4e55680c9e6ba58b00e2f783cf5310e4204f9
= <svilal081@gmail.com>
2024-03-12 15:24:55 +0300

    ???

--- Set/Set/Set.cpp
+++ Set/Set/Set.cpp
@@ -13,14 +13,13 @@ private:
     int size = 0;
     T* Arr = NULL;
 public:
-    MySet() {}
 
     MySet(T* p)
     {
         Arr = p; 
     }
 
-    void setArr(T* p) { Arr = p; }
+
 
     void insert(T item)
     {
@@ -64,6 +63,54 @@ public:
 
     int getSize() { return size; }
 
+    bool count(T item) {
+        for (int i = 0; i < size; i++) {
+            if (Arr[i] == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    MySet& operator * (MySet& s)
+    {
+        int *p = NULL;
+        MySet<T> Arr3{ p };
+        //MySet<T> mySet3;
+
+        for (int i = 0; i < size; i++) {
+            for (int c = 0; c < s.getSize(); c++) {
+                if (i == c && Arr3.count(i) == false) {
+                    Arr3.insert(i);
+                }
+            }
+        }
+        Arr = Arr3;
+        return *this;
+
+    }
+
+    MySet& operator- (T s)
+    {
+        erase(s);
+        return *this;
+    }
+
+    bool operator < (MySet& s)
+    {
+        if (Arr.getSize() < s.Arr.getSize()) {
+            return true;
+        }
+        return false;
+    }
+
+    void show() {
+        for (int i=0; i<size; i++ )
+        {
+            cout << Arr[i] << " ";
+        }
+        cout << endl;
+    };
 };
 
 //class MySet
@@ -130,5 +177,29 @@ int main()
     SetConsoleCP(1251);
 
     int* p = NULL;
-    MySet arr{p};
+    MySet<int> arr{p};
+
+    int* p1 = {0};
+    int* p2 = {0};
+    int* p3 = {0};
+
+    MySet<int> arr1{ p1 };
+    MySet<int> arr2{ p2 };
+    MySet<int> arr3{ p3 };
+
+    for (int i = 0; i < 5; i++) {
+        arr1.insert(i);
+    };
+
+    for (int i = 0; i < 5; i++) {
+        arr2.insert(i);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        arr3.insert(i);
+    }
+
+    arr1 = arr1 - (4);
+    arr1 = arr2 * arr3;
+    arr1.show();
 }
